490568e83e23ed0403ce22c980bf0f2b49a1f0fc
Chen Wang <wangcees@gmail.com>
2019-08-22 17:22:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,14 +6,12 @@ def load_library(file_name)
   emoticons.each do |meaning, emoji|
     hash[:get_meaning][emoji[1]] = meaning
     hash[:get_emoticon][emoji[0]] = emoji[1]
-    
-    
   end
-
   hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_name, emoticon)
+  
   
 end
 
