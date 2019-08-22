12ec96e69781161d5db7053430205d52f0063398
Chen Wang <wangcees@gmail.com>
2019-08-22 17:24:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,13 +5,13 @@ def load_library(file_name)
   emoticons = YAML.load_file(file_name)
   emoticons.each do |meaning, emoji|
     hash[:get_meaning][emoji[1]] = meaning
-    # hash[:get_emoticon][emoji[0]] = emoji[1]
+    hash[:get_emoticon][emoji[0]] = emoji[1]
   end
   hash
 end
 
 def get_japanese_emoticon(file_name, emoticon)
-  
+  emoticons = load_library(file_name)
   
 end
 
